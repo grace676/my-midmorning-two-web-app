73c09f4565b8b5979f96a7c32ea966d7d2a9305f
grace676 <okola676>
2022-07-27 06:21:24 -0400

    eighth commit

---
+++ app/src/main/java/com/haz/mytwopagewebapplication/WebActivity.kt
@@ -0,0 +1,18 @@
+package com.haz.mytwopagewebapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class WebActivity : AppCompatActivity() {
+    var webViewSite:WebView  ?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+        webViewSite = findViewById(R.id.mWebSite)
+        var webSettings = webViewSite!!.settings
+        webSettings.javaScriptEnabled =true
+        webViewSite!!.loadUrl("https://www.emobilis.ac.ke/")
+    }
+}
